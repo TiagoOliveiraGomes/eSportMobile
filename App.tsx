57117f2068e34e51222719e6611aee685cd0fe9b
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-09-20 16:05:47 -0300

    finish nlw mobile

--- App.tsx
+++ App.tsx
@@ -7,11 +7,14 @@ import {
   Inter_900Black
 } from '@expo-google-fonts/inter'
 import Background from './src/components/background';
-import { Home } from './src/screens/home';
 import { Loading } from './src/components/loading';
 import { Routes } from './src/routes';
-
+import * as Notifications from 'expo-notifications'
 import './src/services/notificationConfigs'
+import { getPushNotificationToken } from './src/services/getPushNotificationToken'
+import { useEffect, useRef } from 'react';
+import { Subscription } from 'expo-modules-core'
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,6 +23,30 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black
   })
+  const getNotificationListener = useRef<Subscription>()
+  const responseNotificationListener = useRef<Subscription>()
+  
+  useEffect(()=> {
+    getPushNotificationToken()
+  }, [])
+
+  useEffect(()=> {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    })
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    })
+
+    return () => {
+      if(getNotificationListener.current && responseNotificationListener.current){
+        Notifications.removeNotificationSubscription(getNotificationListener.current)
+        Notifications.removeNotificationSubscription(responseNotificationListener.current)
+      }
+    }
+  })
+
   return (
     <Background >
       <StatusBar
